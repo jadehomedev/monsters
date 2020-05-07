3268562c15c4f34ebcf570e761dd3e8e6b08c9fe
jadehome <jadehomedev@gmail.com>
2020-05-07 17:30:10 +0200

    Updates

---
+++ precache-manifest.60166f498622edf03e08aa6ba040731e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2de470313d80f777ae57693162e30e9",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c80ba12adfe431689d2c",
+    "url": "/monsters/static/css/main.02b9212e.chunk.css"
+  },
+  {
+    "revision": "cb80a15f71da0f888782",
+    "url": "/monsters/static/js/2.668f8c51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.668f8c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80ba12adfe431689d2c",
+    "url": "/monsters/static/js/main.f4cab83b.chunk.js"
+  },
+  {
+    "revision": "3ad366ebbc5ea22c619f",
+    "url": "/monsters/static/js/runtime-main.735d0d8b.js"
+  }
+]);
